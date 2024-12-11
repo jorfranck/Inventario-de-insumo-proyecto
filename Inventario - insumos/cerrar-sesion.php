93b8a8e8b3e5a4a8cb2f1367d91f5cb4998e2b18
jorfranck <129782961+jorfranck@users.noreply.github.com>
2024-12-11 18:36:36 -0400

    Delete Inventario - insumos directory

--- Inventario - insumos/cerrar-sesion.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-session_start();
-
-$_SESSION = [];
-
-var_dump($_SESSION);
-
-header('location: /inicio.php');
--- Inventario - insumos/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="footer">
-    <span>Todos los derechos reservados (Grupo) <?php echo date('d-m-Y'); ?> &copy;</span>
-</footer>
-</body>
-<script src="js/app.js"></script>
-</html>
